3738ed58ff05d1cddc280f428da420edd7bb52fd
LemonNeko <chheese048@gmail.com>
2022-10-06 14:57:58 +0800

    problem solved

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import WalletConnectProvider from "@walletconnect/web3-provider";
+// https://github.com/WalletConnect/walletconnect-monorepo/issues/341
+import WalletConnectProvider from '@walletconnect/web3-provider/dist/umd/index.min.js';
+// instead of
+// import WalletConnectProvider from '@walletconnect/web3-provider';
 
 document.querySelector<HTMLButtonElement>('#connect')!.addEventListener('click', () => {
-  new WalletConnectProvider({
+  const p = new WalletConnectProvider({
     infuraId: '5bd2398e9fcc41c099cbac59722aef62',
   })
+  p.enable()
 })
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// https://github.com/WalletConnect/walletconnect-monorepo/issues/341
+declare module '@walletconnect/web3-provider/dist/umd/index.min.js' {
+    import WalletConnectProvider from '@walletconnect/web3-provider/dist/esm/index';
+    export default WalletConnectProvider 
+}
